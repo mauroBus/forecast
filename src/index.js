e37a73b9f888da2bb8d7592a4daa928c5a00da72
Mauro Buselli <maurobuselli@gmail.com>
2018-12-11 11:46:58 -0300

    Adding a css preset and structuring the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import * as serviceWorker from './serviceWorker'
+import configureStore from './configure-store'
+import App from './App'
+import './index.css'
+import './table.css'
+import '../node_modules/leaflet/dist/leaflet.css'
+import '../node_modules/leaflet/dist/leaflet'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore()
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(app, rootElement)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
