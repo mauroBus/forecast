b80262a398fae6939d8f707223c6c4e699d9aa49
Mauro Buselli <maurobuselli@gmail.com>
2018-12-11 11:48:57 -0300

    Set main table styles and define global constants

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+
+export const API_KEY = '808acf3f3b96ea81dea28eba1b5a4845'
+export const FORECAST_URL = 'http://api.openweathermap.org/data/2.5/weather'
+export const MAP_URL = 'http://maps.openweathermap.org/maps/2.0/weather'
+export const HISTORY_STORAGE_KEY = 'weather-history'
+export const HISTORY_MAX_LENGTH = 5
